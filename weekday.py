81d0058a71e2d4331b0b6d924ae7b876dfd4dfc7
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-03-12 21:54:08 +0000

    test on weekday to see if change

---
+++ weekday.py
@@ -0,0 +1,9 @@
+import datetime
+#allows you to use current date/day/time in following code
+
+while datetime == range(0, 5):
+    #asses if it is 0-5 (Mon - Fri with dateime) and prints accordingly
+    print("Yes, unfortunately today is a weekday")
+print("It is the weekend, yay!")
+
+#test on weekday to see if works
