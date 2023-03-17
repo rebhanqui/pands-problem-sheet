6768ce8411d6511f3220808379afcc7cbde0372f
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-03-17 17:21:46 +0000

    additional comments on task 6

--- squareroot.py
+++ squareroot.py
@@ -1,7 +1,8 @@
-#REF 1: https://www.cuemath.com/algebra/squares-and-square-roots/
+#REF 1: http://www.andreamarino.it/python/thinkcspy/MoreAboutIteration/Newton%27sMethod.html
+# defining numbers as whatever is input by user
 numbers = int(input("Please pick a number: "))
 
-
+#function to apply the newton method of getting the square root
 def newtonSqrt(numbers):
     approx = 0.5 * numbers
     better = 0.5 * (approx + numbers/approx)
@@ -11,9 +12,10 @@ def newtonSqrt(numbers):
     better = 0.5 * (approx + numbers/approx)
 
     return approx
+#allows result of input calculate by .5 to be further calculated to get the sqrt
 
 print(f"The Square Root of {numbers} is approx. {newtonSqrt(numbers)}")
-
+#prints th euser input number and the result of the function
 
     
     
