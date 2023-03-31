1c900d3caabbf03fd6c8d8673b82df50c221f0d1
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-03-31 18:48:42 +0100

    completed week 7 task, rewrote code to better understand

--- readecount.py
+++ readecount.py
@@ -1,1 +1,24 @@
+#SCRIPT TO COUNT HOW MANY E'S IN A TXT FILE
+#Author: Rebecca Quinn
+
+#permanent file path
+FILENAME = "/Users/rebeccaquinn/Desktop/pands-problem-sheet/countfile.txt"
+
+#opens file and reads it with 'r'
+file = open(FILENAME, 'r')
+
+#puts file content to string
+data = file.read()
+
+#requests user to select a letter
+letter = input("Pick a letter to see how many occurences are in the count file: ")
+
+#count counts how many times the chosen letter occurs
+occurences = data.count(letter)
+
+#prints result
+print(f"There are {occurences} letter {letter}'s in the count file")
+
+#REF 1: https://pythonexamples.org/python-count-occurrences-of-word-in-text-file/
+#REF 2: https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
 
