e8fae4cbc35d59876f82b878853a6fec9453ba2b
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-04-04 19:39:24 +0100

    week 8 task complete

--- plotting.py
+++ plotting.py
@@ -9,29 +9,28 @@ meanvalue = 5
 standard = 2
 distrib = 1000
 x = np.random.normal(meanvalue, standard, distrib)
-plt.hist(x)
-
 
 xpoints = np.arange(0,11)
 
 #x (xpoints) to the power of 3 using pow REF: https://www.geeksforgeeks.org/python-pow-function/
 ypoints = pow(xpoints, 3,)
 
-plt.plot(xpoints, ypoints)
-plt.plot(xpoints, color = ("cyan"), edgecolor = ("w"))
-plt.plot(ypoints, color = ("magenta"), edgecolor = ("black"))
-
 #CUSTOMISING PLOT CODE BELOW]
-
+#REF https://medium.com/@arseniytyurin/how-to-make-your-histogram-shine-69e432be39ca
+plt.figure(figsize=(14,7)) # size of fig 14x7
+plt.style.use('seaborn-whitegrid') #grid style
 plt.title("Plotting Plot")
 plt.xlabel("X Data")
 plt.ylabel("Y Data")
-# draw solid white grid lines
+plt.hist(x, facecolor="cyan", edgecolor="white") # draw solid white grid lines
+plt.plot(xpoints, ypoints, linewidth=5.0, color="pink") #https://www.statology.org/matplotlib-line-thickness/
+
 #REF: https://jakevdp.github.io/PythonDataScienceHandbook/04.11-settings-and-stylesheets.html
-plt.grid()
+
 
 #shows the plots
-# saves the plot into an image file as seen in plotted.png plt.savefig("plotted.png")
+# saves the plot into an image file as seen in plotted.png 
+#plt.savefig("plotted.png")
 # REF https://towardsdatascience.com/save-plots-matplotlib-1a16b3432d8a
 plt.show()
 
